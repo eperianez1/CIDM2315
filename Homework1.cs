5157b741cf38006fc7acd66c276720f02aa9c486
eperianez1 <eperianez1@buffs.wtamu.edu>
2024-09-06 17:02:18 -0700

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5, y = 3.3;
+        Console.WriteLine($"X = {x}, Y = {y}");
+        double z = (4*x*x)+(3*y);
+        Console.WriteLine($"The value of Z is: {z}");
+
+
+    }
+}
